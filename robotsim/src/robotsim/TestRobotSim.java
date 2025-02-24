4585ada800666dc1ec52cafbffabe1cbdae20ea6
Issa DAOU <daouissa7@gmail.com>
2025-02-24 22:54:57 +0100

    ajout

--- robotsim/src/robotsim/TestRobotSim.java
+++ robotsim/src/robotsim/TestRobotSim.java
@@ -5,6 +5,7 @@ public class TestRobotSim {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Robot myRobot= new Robot("Robot 1", 5);
+		Robot r2= new Robot("Robot 2", 10);
 		System.out.print(myRobot.toString());
 	}
 
