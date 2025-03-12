35a204bc6bcb6d7e91d70834299956f4dea0f19d
Issa DAOU <daouissa7@gmail.com>
2025-03-12 09:23:30 +0100

    tp3

--- robotsim/src/robotsim/TestRobotSim.java
+++ robotsim/src/robotsim/TestRobotSim.java
@@ -1,15 +1,19 @@
 package robotsim;
 
+import java.util.ArrayList;
+
+import fr.tp.robotsim.model.Factory;
+import fr.tp.robotsim.model.Robot;
+
 public class TestRobotSim {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Robot myRobot= new Robot("Robot 1", 5);
 		Robot r2= new Robot("Robot 2", 10);
-		System.out.print(myRobot.toString());
+		Factory f= new Factory("monUsine",new ArrayList<Robot>());
+		f.addRobot("r1"); f.addRobot("r3");
+		f.printToConsole();
 	}
-
-	
-	
 
 }
